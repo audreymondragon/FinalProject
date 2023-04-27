7fc879a64d19d32a53df94173ba7de797120e47d
Audrey Mondragon <audreymondragon@Audreys-MBP.lan>
2023-04-27 10:00:58 -0700

    Updated html files and server.py

--- crud.py
+++ crud.py
@@ -4,9 +4,28 @@ from model import db, User, Preference, Restaurant, Visited, Favorite, connect_t
 def create_user(username, email, password):
     """Create and return a new user"""
 
+    print(f'Creating user: {username}, {email}, {password}')
     user = User(username=username, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    print(f"Created user with ID: {user.user_id}")
 
     return user
+
+def get_users():
+    """Return all users"""
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """Return a user by email"""
+
+    return User.query.filter(User.email == email).first()
 
 
 def create_preference(user, cuisine_type, min_yelp_rating, min_yelp_price, max_distance):
@@ -16,6 +35,17 @@ def create_preference(user, cuisine_type, min_yelp_rating, min_yelp_price, max_d
 
     return preference
 
+def get_preferences():
+    """Return all preferences"""
+
+    return Preference.query.all()
+
+def get_preference_by_id(preference_id):
+    """Return a preference by primary key."""
+
+    return Preference.query.get(preference_id)
+
+
 
 def create_restaurant(restaurant_name, yelp_rating, yelp_price, location):
     """Create and return a user's restaurants"""
@@ -24,6 +54,18 @@ def create_restaurant(restaurant_name, yelp_rating, yelp_price, location):
 
     return restaurant
 
+def get_restaurants():
+    """Return all restaurants"""
+
+    return Restaurant.query.all()
+
+def get_restaurant_by_id():
+    """Return a restaurant by primary key"""
+
+    return Restaurant.query.get(restaurant_id)
+
+
+
 def create_visited(user, restaurant, visited, wishlist, location, yelp_api_restaurant_id):
     """Create and return a user's visited restaurants"""
 
@@ -31,12 +73,36 @@ def create_visited(user, restaurant, visited, wishlist, location, yelp_api_resta
 
     return visited
 
+def get_visited():
+    """Return all visited restaurants"""
+
+    return Visited.query.all()
+
+def get_visited_by_id():
+    """Return a visited restaurant by primary key"""
+
+    return Visited.query.get(user_restaurant_id)
+
+
+
 def create_favorite(user, restaurant):
     """Create and return a user's favorite restaurants"""
 
     favorite = Favorite(user=user, restaurant=restaurant)
 
     return favorite
+
+def get_favorite():
+    """Return all favorite restaurants"""
+
+    return Favorite.query.all()
+
+def get_favorite_by_id():
+    """Return a favorite restaurant by primary key"""
+
+    return Favorite.query.get(fav_restaurant_id)
+
+
     
 if __name__ == '__main__':
     from server import app
--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
 from model import connect_to_db, db, User, Preference, Restaurant, Visited, Favorite
-#import crud
+import crud
 
 from jinja2 import StrictUndefined
 
@@ -11,6 +11,99 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    """View homepage"""
+
+    return render_template('homepage.html')
+
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def new_account():
+    """Create a new account."""
+
+    if request.method =='POST':
+        # gets the form variables
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # if user already exists
+        user = crud.get_user_by_email(email)
+
+        if user:
+            flash('That email is already in use, please try again.')
+            return redirect('/create_account')
+
+        # create and add new user to db
+        user = crud.create_user(username, email, password)
+        
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Account created successfully! Please log in.')
+        return redirect('/login')
+    
+    return render_template('create_account.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_process():
+    """Process the user login"""
+    if request.method == 'POST':
+        #QUESTION: should I remove the username?
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # if user exists
+        user = crud.get_user_by_email(email)
+   
+        if not user or user.password != password:
+            flash("The email or password you entered was incorrect.")
+        else:
+            session["user_email"] = user.email
+            flash(f"Welcome, {user.email}!")
+
+            return redirect('/preferences')
+
+    return render_template('login.html')
+
+
+@app.route('/preferences', methods=['GET'])
+def preferences():
+    """Displays the page to enter user preferences in the form"""
+
+    return render_template('preferences_form.html')
+
+@app.route('/preferences', methods=['POST'])
+def preferences_form():
+    """Process the user's preferences from the form"""
+    # Get user's preferences from form
+    # QUESTION: do these need "preference." before them to be an attribute of the instance of the Preference class?
+    cuisine_type = request.form.get('cuisine_type')
+    min_yelp_rating = request.form.get('min_yelp_rating')
+    min_yelp_price = request.form.get('min_yelp_price')
+    max_distance = request.form.get('max_distance')
+
+    db.session.commit()
+
+    flash ('Preferences submitted successfully!')
+    return redirect('/recommendations')
+
+@app.route('/recommendations')
+def restaurant_recommendations():
+    """Displays recommendations based on the user's preferences"""
+
+    # get user's preferences
+    preference = crud.get_preference_by_id(preference_id)
+
+    # code here to call Yelp API
+
+    # code here to call Google Maps API
+
+    # code here to combine the 2?
+    return render_template('recommendations.html')
 
 if __name__ == "__main__":
     connect_to_db(app)
